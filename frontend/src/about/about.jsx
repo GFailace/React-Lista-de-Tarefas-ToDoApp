7c5967eff83b72243c321a2574c93a93e5f8760d
Guilherme Failace <guilherme.failace@gmail.com>
2018-09-11 02:27:26 -0300

    Projeto toDoApp

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre'></PageHeader>
+        
+        <h2>Utilização</h2>
+        <p>Este app pode ser utilizado para organizar suas tarefas do dia a dia.</p>
+    </div>
+)
